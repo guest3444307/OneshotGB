55ffc2efa52169a593301a4a1cc41611f5021202
Max Nexus <guest3444307@gmail.com>
2022-07-26 18:36:26 -0400

    7/26/22 Coloring and Titlescreen update
    7/26/22 part 2: Finished that title screen for GBC and GB maybe i could adjust the text in the GBC since its not particularly good colors since i changed the pallete. I'm going to work on the UI tommorow as i hadn't thought of changing the default UI until now. I made a temp asset from resizing and modifying from the original game which i'll get around to converting
    
    7/26/22 Coloring and Titlescreen update
    
    7/26/22 part 2: Finished that title screen for GBC and GB maybe i could adjust the text in the GBC since its not particularly good colors since i changed the pallete. I'm going to work on the UI tommorow as i hadn't thought of changing the default UI until now. I made a temp asset from resizing and modifying from the original game which i'll get around to converting

---
+++ Experimental Assets/titlescreen gb/Normal GB Temp.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="Normal GB Temp" tilewidth="8" tileheight="8" tilecount="360" columns="20">
+ <image source="../../assets/backgrounds/Normal GB Temp.png" width="160" height="144"/>
+</tileset>
